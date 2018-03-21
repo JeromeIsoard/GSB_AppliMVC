2ee8ba5cb1e36d3c69c72c2db779c1f4b8970c3a
Jerome Isoard <isoard.jerome@gmail.com>
2018-03-21 23:13:53 +0100

    - création d'une fonction getInfosComptable (permet de se connecter avec un compte comptable). - modifiction du case 'valideConnexion' dans le c_connexion pour prendre en compte la connexion d'un comptable.

--- controleurs/c_connexion.php
+++ controleurs/c_connexion.php
@@ -27,16 +27,26 @@ case 'valideConnexion':
     $login = filter_input(INPUT_POST, 'login', FILTER_SANITIZE_STRING);
     $mdp = filter_input(INPUT_POST, 'mdp', FILTER_SANITIZE_STRING);
     $visiteur = $pdo->getInfosVisiteur($login, $mdp);
-    if (!is_array($visiteur)) {
-        ajouterErreur('Login ou mot de passe incorrect');
-        include 'vues/v_erreurs.php';
-        include 'vues/v_connexion.php';
-    } else {
+    $comptable = $pdo->getInfosComptable($login, $mdp);
+    //test si c'est un visiteur, un comptable ou aucun des deux qui se connecte
+    if (is_array($visiteur)) {
         $id = $visiteur['id'];
         $nom = $visiteur['nom'];
         $prenom = $visiteur['prenom'];
         connecter($id, $nom, $prenom);
         header('Location: index.php');
+    } elseif (is_array($comptable)) {
+        $id = $comptable['id'];
+        $nom = $comptable['nom'];
+        $prenom = $comptable['prenom'];
+        //déclaration d'une variable estComptable
+        $estComptable = TRUE;
+        connecter($id, $nom, $prenom);
+        header('Location: index.php');
+    }else{
+        ajouterErreur('Login ou mot de passe incorrect');
+        include 'vues/v_erreurs.php';
+        include 'vues/v_connexion.php';
     }
     break;
 default:
